d68825da6c91da1baeae49094e57cb19faf65f12
totalsuccess <157171259+totalsuccess@users.noreply.github.com>
2025-03-01 07:38:17 +0100

    finished js logic and optimized js

--- app.js
+++ app.js
@@ -1,4 +1,64 @@
 let form = document.querySelector('form');
-let inputs = form.querySelectorAll('input');
+let resetBtn = document.getElementById('tip_calc_reset_btn')
+let peopleError = document.getElementById('tip_calc_error')
+const tipData = {
+    bill: 0,
+    tip: 0,
+    people: 0
+};
 
-console.log(inputs); // null
+function updateTipData(name, value) {
+    tipData[name] = name === 'custom_tip' ? parseFloat(value) / 100 || 0 : parseFloat(value) || 0;
+    calculateTip();
+}
+
+function isPeopleValid() {
+    return tipData.people > 0;
+}
+
+function handleError() {
+    peopleError.classList.toggle('active', !isPeopleValid());
+}
+
+function renderTipStats(tipPerPerson, totalPerPerson) {
+    document.querySelector('.tip_amount_per_person_value').textContent = `$${tipPerPerson.toFixed(2)}`;
+    document.querySelector('.tip_amount_total_value').textContent = `$${totalPerPerson.toFixed(2)}`;
+}
+
+function reset() {
+    Object.assign(tipData, { bill: 0, tip: 0, people: 0 });
+    form.reset();
+
+    document.getElementById('people').value = ''; // Manually clear the input to avoid validation issue
+    
+    peopleError.classList.remove('active');
+    renderTipStats(0.00, 0.00);
+}
+
+
+function calculateTip() {
+    if (isPeopleValid() && tipData.bill > 0) {
+        let tipPerPerson = (tipData.bill * tipData.custom_tip) / tipData.people;
+        let totalPerPerson = (tipData.bill * (1 + tipData.custom_tip)) / tipData.people;
+        renderTipStats(tipPerPerson, totalPerPerson);
+    } else {
+        renderTipStats(0.00, 0.00);
+    }
+}
+
+function handleInput(e) {
+    updateTipData(e.target.name, e.target.value);
+    handleError();
+}
+
+function handleBtnClick(e) {
+    if (e.target.matches('.tip_selector[type="button"]')) {
+        updateTipData('custom_tip', e.target.value.replace('%', ''));
+        handleError()
+    }
+}
+
+form.addEventListener('input', handleInput);
+form.addEventListener('click', handleBtnClick);
+
+resetBtn.addEventListener('click', reset);
